aef7e13d3167a0e2d0eb425e128d891130a048f4
akhil murali <akhilmurali662@gmail.com>
2022-01-13 11:28:07 +0530

    configuration file added

---
+++ src/main/java/com/tigerlogistics/LogisticstigerApplication.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticstigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticstigerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tigerlogistics/LogisticstigerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticstigerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
